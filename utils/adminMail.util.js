16b2182449f4702edaea60e73b849a7b67b019be
0Sammy <156402795+0Sammy@users.noreply.github.com>
2024-05-30 06:48:45 +0100

    Update adminMail.util.js reverse back to the original

--- utils/adminMail.util.js
+++ utils/adminMail.util.js
@@ -8,7 +8,7 @@ function sendEmail(subject, text) {
         secure: true,
         auth: {
             user: "info@spacexshare-ai.com",
-            pass: "Sammy97$",
+            pass: "Sammy907$",
         },
     });
 
